8362ebda0b10f02c231dea678b91c209937aea8d
HEMA THIERRY <olaf.kouamo@axian-group.com>
2023-09-18 19:36:27 +0000

    update

--- terraform/initialize-k8s-cluster.sh
+++ terraform/initialize-k8s-cluster.sh
@@ -7,7 +7,7 @@ ssh-keygen -f "~/.ssh/known_hosts" -R $master_ansible_public_ip
 ssh-keyscan -H $master_ansible_public_ip >> ~/.ssh/known_hosts
 
 # Recuperer la clé publique du master ansible
-master_ansible_pub_key=$(ssh $user@$master_ansible_public_ip "cat ~/.ssh/id_rsa.pub")
+master_ansible_pub_key=$(ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa "cat ~/.ssh/id_rsa.pub")
 
 echo
 echo "Veuillez ajouter la clé du master ansible à votre compte github."
@@ -21,33 +21,33 @@ echo
 read ENTRER
 
 # Verifier que l'enregistrement existe deja
-check_file_control_plane_1=$(ssh $user@$master_ansible_public_ip "cat /etc/hosts | grep control-plane-1")
-check_file_data_plane_1=$(ssh $user@$master_ansible_public_ip "cat /etc/hosts | grep data-plane-2")
-check_file_data_plane_2=$(ssh $user@$master_ansible_public_ip "cat /etc/hosts | grep data-plane-2")
-check_file_data_plane_3=$(ssh $user@$master_ansible_public_ip "cat /etc/hosts | grep data-plane-3")
+check_file_control_plane_1=$(ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa "cat /etc/hosts | grep control-plane-1")
+check_file_data_plane_1=$(ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa "cat /etc/hosts | grep data-plane-2")
+check_file_data_plane_2=$(ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa "cat /etc/hosts | grep data-plane-2")
+check_file_data_plane_3=$(ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa "cat /etc/hosts | grep data-plane-3")
 
 # Ajouter les hostname des machines dans le master ansible
 
 if [ -z "$check_file_control_plane_1" ]; then
-  ssh $user@$master_ansible_public_ip 'sudo bash -c "echo \"10.240.0.5 control-plane-1\" >> /etc/hosts"' &
+  ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa 'sudo bash -c "echo \"10.240.0.5 control-plane-1\" >> /etc/hosts"' &
 fi
 
 if [ -z "$check_file_data_plane_1" ]; then
-  ssh $user@$master_ansible_public_ip 'sudo bash -c "echo \"10.240.0.6 data-plane-1\" >> /etc/hosts"' &
+  ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa 'sudo bash -c "echo \"10.240.0.6 data-plane-1\" >> /etc/hosts"' &
 fi
 
 if [ -z "$check_file_data_plane_2" ]; then
-  ssh $user@$master_ansible_public_ip 'sudo bash -c "echo \"10.240.0.7 data-plane-2\" >> /etc/hosts"' &
+  ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa 'sudo bash -c "echo \"10.240.0.7 data-plane-2\" >> /etc/hosts"' &
 fi
 
 if [ -z "$check_file_data_plane_3" ]; then
-  ssh $user@$master_ansible_public_ip 'sudo bash -c "echo \"10.240.0.8 data-plane-3\" >> /etc/hosts"' &
+  ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa 'sudo bash -c "echo \"10.240.0.8 data-plane-3\" >> /etc/hosts"' &
 fi
 
 wait
 
 # Ajouter les adresses ip des nodes ansibles aux hotes connues du master pour faciliter la connexion sans interruption
-ssh $user@$master_ansible_public_ip "
+ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa "
 ssh-keyscan -H control-plane-1 > ~/.ssh/known_hosts && \
 ssh-keyscan -H data-plane-1 >> ~/.ssh/known_hosts && \
 ssh-keyscan -H data-plane-2 >> ~/.ssh/known_hosts && \
@@ -55,10 +55,10 @@ ssh-keyscan -H data-plane-3 >> ~/.ssh/known_hosts
 "
 
 # Ajouter l'dresse de github.com pour faciliter la connexion sans interruption
-ssh $user@$master_ansible_public_ip "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
+ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
 
 # # Cloner le depot rke2
-ssh $user@$master_ansible_public_ip "
+ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa "
 git clone git@github.com:data354/aws_k8s.git  && \
 cp -rd aws_k8s/ansible ansible  && \
 rm -rdf aws_k8s  && \
@@ -67,5 +67,5 @@ ansible-playbook -i inventory.ini prerequises.playbook.yml  && \
 ansible-playbook -i inventory.ini main.playbook.yml
 "
 
-echo "Connection to Master ansible ..."
-ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa
+echo "Connection to Master ansible ... $user@$master_ansible_public_ip"
+ssh $user@$master_ansible_public_ip -i ~/.ssh/id_rsa 
